402290a7ef45eb7d032ad480025629f231b18428
adc work <adc9work@outlook.com>
2020-07-16 12:04:56 +0530

    Passing the Value Data Using Context Child to Parent

---
+++ src/context/BlogContext.js
@@ -0,0 +1,9 @@
+import React, { Children } from "react";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = (childern) => {
+  return <BlogContext.Provider value={5}>{childern}</BlogContext.Provider>;
+};
+
+export default BlogContext;
